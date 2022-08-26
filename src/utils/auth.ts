b36fff171a9fe80512fa20717e7351a877e1335d
Guilherme Pereira <guilherme.rp2010@hotmail.com>
2022-08-26 16:08:45 +0100

    Add authentication header decoder to `utils`

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,1 +1,16 @@
+import * as jwt from 'jsonwebtoken'
+
 export const APP_SECRET = 'POTATO'
+
+export interface AuthTokenPayload {
+  userId: number
+}
+
+export function decodeAuthHeader(authHeader: string): AuthTokenPayload {
+  const token = authHeader.split('Bearer ')[1]
+
+  if (!token) { throw new Error("No token found") }
+
+  return jwt.verify(token, APP_SECRET) as AuthTokenPayload
+}
+
